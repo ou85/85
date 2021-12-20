29aa194486800f87533f20e498a2c08aa7b02afc
mironov <tech@toasoft.co.jp>
2021-12-20 17:23:52 +0900

    per hour

--- scripts.js
+++ scripts.js
@@ -1,2 +1,24 @@
 document.write("<h2>Pushing JavaScript</h2></br>");              // выводим заголовок
 document.write("Hello World!</br>");                             // выводим обычный текс
+
+
+
+
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  }
+  
+/* Try running this in the console; then test with several arguments. For example:
+
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5, 3);
+*/
+
+// Daily income
+function dayRate(ratePerHour) {
+    return 8 * ratePerHour;
+  }
+
