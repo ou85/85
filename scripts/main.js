4189b0691ca7686dfb73a25f1ab5c1197f731aec
mironov <tech@toasoft.co.jp>
2021-12-21 14:44:01 +0900

    Minor fix

--- scripts/main.js
+++ scripts/main.js
@@ -22,16 +22,20 @@ var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
-    var myName = promt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozill is cool, ' + myName;
+    var myName = prompt('Please enter your name.');        
+    if(!myName) {
+       setUserName();
+    }  else {     
+       localStorage.setItem('name', myName);
+       myHeading.textContent = 'Mozill is cool, ' + myName;
+    }
 }
 
 if(!localStorage.getItem('name')) {
     setUserName();
   } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozillf is cool, ' + storedName;
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
