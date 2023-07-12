d7cc76a9e3a339b4511766a0fd14723f11daded8
“YourName” <never@mail.me>
2023-07-12 17:49:23 +0900

    Rolling

--- webgallery/script.js
+++ webgallery/script.js
@@ -31,8 +31,8 @@ setInterval(updateClock, 1000);
 
 // Set up photo grid
 const photoGrid = document.getElementById("photo-grid");
-let imageIndexes = [];
-let amountOfPicures = 140; // <============== Amount of pictures in folder "Pictures"
+let imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let amountOfPicures = 131; // <============== Amount of pictures in folder "Pictures"
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -52,29 +52,46 @@ function changeRandomImage() {
 function checkDouble() {
   const randomImageIndex = getRandomInt(1, amountOfPicures);
 
-  imageIndexes.includes(randomImageIndex)
-    ? console.log("Image already in use: " + randomImageIndex)
-    : imageIndexes.push(randomImageIndex);
+  if (!imageIndexes.includes(randomImageIndex)) {
+    imageIndexes.push(randomImageIndex);
+    imageIndexes.length > 9 ? imageIndexes.shift() : null;
+    // imageIndexes.length == 40 ? (imageIndexes = []) : null;
+  } else {
+    console.log(
+      "Image already in use: " +
+        randomImageIndex +
+        "\n" +
+        "Use image: " +
+        imageIndexes[0] +
+        " or " +
+        imageIndexes[1] +
+        " instead"
+    );
 
-  imageIndexes.length == 50 ? (imageIndexes = []) : null;
+    return randomImageIndex == imageIndexes[0]
+      ? imageIndexes[1]
+      : imageIndexes[0];
+  }
 
-  console.log("Array: " + imageIndexes);
   console.log("Image: " + imageIndexes[imageIndexes.length - 1]);
+  console.log("Array lenght: " + imageIndexes.length);
+  console.log("Array: " + imageIndexes);
 
-  return randomImageIndex;
+  // return randomImageIndex;
+  return imageIndexes[imageIndexes.length - 1];
 }
 
-setInterval(changeRandomImage, getRandomInt(5000, 10000));
+setInterval(changeRandomImage, getRandomInt(9000, 50000));
 
 // Load initial images
-for (let i = 0; i < 9; i++) {
+for (let i = 1; i < 10; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
 
   const link = document.createElement("a");
   const image = document.createElement("img");
-  const randomImageIndex = checkDouble(); //getRandomInt(1, amountOfPicures);
-  const imageUrl = `Pictures/${randomImageIndex}.jpg`;
+  //const randomImageIndex = checkDouble(); // getRandomInt(1, amountOfPicures);
+  const imageUrl = `Pictures/${i}.jpg`; // `Pictures/${randomImageIndex}.jpg`;
   image.setAttribute("src", imageUrl);
   link.setAttribute("href", imageUrl);
   // Random picture link
